c7ff7bcec91dd3d71982bfed9a7fc06789027b15
Liuziyu <1280217255@qq.com>
2022-06-30 16:42:01 +0800

    third commit

--- SpringBoot_demo1/src/main/java/edu/xttc/java/HelloController.java
+++ SpringBoot_demo1/src/main/java/edu/xttc/java/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello Spring Boot Git1";
+        return "hello Spring Boot Git12";
     }
 }
